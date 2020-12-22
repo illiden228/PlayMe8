37ba4db566d0ecb834f0cba505ecc98add35b96b
Денис <den.khabibullin@gmail.com>
2020-12-22 16:37:18 +0300

    Почти готов дизайн интерфейса (нужна пара корректировок) Написал скрипт загрузки изображений из интернета

---
+++ Assets/ImageLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scripts : MonoBehaviour
+{
+    public RawImage Img;
+
+    private IEnumerator Start()
+    {
+        WWW www = new WWW("https://i.pinimg.com/564x/4a/bc/c0/4abcc00427dbb86ee5da8270b52204f8.jpg");
+        yield return www;
+        Img.texture = www.texture;
+    }
+}
